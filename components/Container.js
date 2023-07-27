52ba8fffc66ce1cdb0ada8c929836d874d4834b0
hjsjhn <hjsjhn@outlook.com>
2023-07-27 22:00:29 +0800

    Changed gradient styles & footer

--- components/Container.js
+++ components/Container.js
@@ -19,9 +19,9 @@ export default function Container(props) {
 
             <footer className={styles.footer}>
                 <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
-                    Powered by {"TankNee"}
+                    Based on {"TankNee"} <br>
                     <span className={styles.logo}>
-                        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                        Deployed on <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
                     </span>
                 </a>
             </footer>
